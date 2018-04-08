2cbf825bc48ae95f39fcfd70d87e79d6e5f54bbd
kykorman <kykorman@gmail.com>
2018-04-08 15:19:55 -0500

    Currently testing network connectivity

--- enigma.cpp
+++ enigma.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
 #include <random>
-using namespace std;
+#include <fstream>
 
+using namespace std;
+unsigned char *fgetus(unsigned char *dst, int max, FILE *fp);
+//void send(unsigned char msg[]);
+//void receive(unsigned char encrypted[]);
+void sendMain(int r1[],int r2[],int r3[],int r1Pos,int r2Pos,int r3Pos);
+void receiveMain(int r1[],int r2[],int r3[],int r1Pos,int r2Pos,int r3Pos);
 
 void encode(int r1[],int r2[], int r3[], int r1Pos,int r2Pos, int r3Pos, string msg, unsigned char msgEncrypted[]);
 void decode(int r1[],int r2[], int r3[], int r1Pos,int r2Pos, int r3Pos, unsigned char msgEncrypted[], unsigned char msgUnencrypted[]);
+
 int strlen(unsigned char msg[]);
+
 int validInput(char word[30]);
 int rotorNotUnique(int rotor[255],int spot);
 void print_rotor(int rotor[255]);
@@ -28,7 +36,7 @@ int main(){
 	for(int i=0;i<1000;i++)//use the generator a bit so that the numbers start to deviate more, this is an issue with mersenne's twister algorithm that I saw.	
 		gen_rotor(generator);
 
-//Make rotors all unique, not duplicate values
+//Generate rotors randomly, ensure no duplicates on the same rotor.
 	for(int i=0;i<255;i++)
 		do
 			r1[i]=gen_rotor(generator);
@@ -51,27 +59,21 @@ int main(){
 */
 
 	char thing4[50]="0 to decode, 1 or higher to encode: ";
-//	int encodeOrNah=validInput(thing4);
+	int encodeOrNah=validInput(thing4);
 
-	
-//	if(encodeOrNah){
-		string msg,jnk;
-	
-		cout<<"Enter message to encrypt, under 1472 characters: ";
+	system("clear");
+	if(encodeOrNah)
+		sendMain(r1,r2,r3,r1Pos,r2Pos,r3Pos);
+/*
+*/
+	else
 
-		getline(cin,jnk);//Consume all the garbage
-		getline(cin,msg);
-		unsigned char msgEncrypted[msg.length()+1];
-
-		encode(r1,r2,r3,r1Pos,r2Pos,r3Pos,msg,msgEncrypted);
-//	}else{
+		receiveMain(r1,r2,r3,r1Pos,r2Pos,r3Pos);
+/*
 		unsigned char msgUnencrypted[strlen(msgEncrypted)+1];
 		decode(r1,r2,r3,r1Pos,r2Pos,r3Pos,msgEncrypted,msgUnencrypted);
-//	}
-	cout<<"\nUnencrypted message \n\n\n";
-	for(int i=0;i<msg.length();i++)
-		cout<<msgUnencrypted[i];
-	cout<<endl;
+*/
+	
 //cout<<(string)msgUnencrypted<<endl;
 return 0;}
 void decode(int r1[],int r2[], int r3[], int r1Pos,int r2Pos, int r3Pos, unsigned char msgEncrypted[], unsigned char msgUnencrypted[]){
@@ -172,3 +174,160 @@ int strlen(unsigned char msg[]){
 		len++;
 
 return len;}
+
+void sendMain(int r1[],int r2[],int r3[],int r1Pos,int r2Pos,int r3Pos){
+	system("gcc client.cpp -o c.out");
+
+	int portno;
+
+	ofstream msgFile;
+	msgFile.open("tmpmsg");
+	string msg,jnk;
+
+	cout<<"Enter message to encrypt, under 1472 characters: ";
+
+	getline(cin,jnk);//Consume all the garbage
+	getline(cin,msg);
+	unsigned char msgEncrypted[msg.length()+1];
+
+	cout<<"Enter port number to send on (1-65535): ";
+	cin>>portno;
+	cout<<portno<<endl;
+	cout<<"Enter host to send to: ";
+
+	char jnk2;
+	scanf("%c",&jnk2);
+	char serveraddr[100]="";
+	cin>>serveraddr;
+
+	encode(r1,r2,r3,r1Pos,r2Pos,r3Pos,msg,msgEncrypted);
+	msgFile<<msgEncrypted;
+	string cmd="./c.out ";
+
+	cmd+=serveraddr;
+	cmd+=" ";
+	cmd+=to_string(portno);
+	cout<<cmd<<endl;
+	char newcmd[cmd.length()+1];
+	strcpy(newcmd,cmd.c_str());
+
+	system(newcmd);
+	cout<<"Sent\n";
+//	system("rm c.out;rm tmpmsg");
+}
+
+void receiveMain(int r1[],int r2[],int r3[],int r1Pos,int r2Pos,int r3Pos){
+	unsigned char encrypted[1473]="",unencrypted[1473]="";
+	int portno;
+	
+	system("g++ server.cpp -o b.out");
+	
+	cout<<"Enter port number to listen on (1-65535): ";
+	cin>>portno;
+
+	string cmd="./b.out ";
+	cmd+=portno;
+
+	char newcmd[cmd.length()+1];
+	strcpy(newcmd,cmd.c_str());
+
+	system(newcmd);
+
+	FILE *encMsg=fopen("out","r");
+	fgetus(encrypted,1472,encMsg);	
+
+	decode(r1,r2,r3,r1Pos,r2Pos,r3Pos,encrypted,unencrypted);
+
+	for(int i=0;i<strlen(unencrypted);i++)
+		printf("%c",unencrypted[i]);
+	cout<<endl;	
+	cout<<"1 to receive with same parameters, 0 to exit";
+	int tmp;
+	cin>>tmp;
+
+	if(tmp)
+		receiveMain(r1,r2,r3,r1Pos,r2Pos,r3Pos);
+	else{
+//		system("rm b.out");
+//		system("rm out");
+		exit(0);
+	}
+}
+/*
+void send(unsigned char msg[]){
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	char serveraddr[20]="";
+	cout<<portno<<endl;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	cout<<serveraddr<<endl;
+	server = gethostbyname(serveraddr);
+	cout<<server<<endl;;
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+		(char *)&serv_addr.sin_addr.s_addr,
+		server->h_length);
+	serv_addr.sin_port = htons(portno);
+	n = write(sockfd,msg,strlen(msg));
+	close(sockfd);
+}
+
+
+void receive(unsigned char encrypted[]){
+	int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    char thing[50]="Enter port number to listen on (1-65535): ";
+	portno=validInput(thing);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+	listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, 
+		(struct sockaddr *) &cli_addr, 
+		&clilen);
+	bzero(encrypted,1473);
+	n = read(newsockfd,encrypted,1472);
+    printf("Encrypted message: %s\n",encrypted);
+
+    close(newsockfd);
+    close(sockfd);
+}
+*/
+unsigned char *
+fgetus(unsigned char *dst, int max, FILE *fp)
+{
+	int c;
+	unsigned char *p;
+
+	/* get max bytes or upto a newline */
+
+	for (p = dst, max--; max > 0; max--) {
+		if ((c = fgetc (fp)) == EOF)
+			break;
+		*p++ = c;
+		if (c == '\n')
+			break;
+	}
+	*p = 0;
+	if (p == dst || c == EOF)
+		return NULL;
+	return (p);
+}
+
---
+++ server.cpp
@@ -0,0 +1,58 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fstream> 
+using namespace std;
+void error(const char *msg)
+{
+    printf("%s\n",msg);
+    exit(1);
+}
+ 
+int main(int argc, char *argv[])
+{
+	ofstream out;
+	out.open("out");
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     unsigned char buffer[1473];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,1473);
+     n = read(newsockfd,buffer,1472);
+     if (n < 0) error("ERROR reading from socket");
+	out<<buffer<<endl;
+     close(newsockfd);
+     close(sockfd);
+	out.close();
+//	system("rm out");
+     return 0; 
+}
